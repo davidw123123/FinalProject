63a4677734a81ee610b2137ecf89aeb644432dea
BT_2S14_08 <davidwg1210@gmail.com>
2025-05-21 10:57:31 -0400

    Wrote down the basic algorithm for the minimax method. I added 3 more methods that is needed for the algorithm. Also need to change the structure of the AI class so that the methods are able to be used in the ConnectFour class. Change all to static and work on the other methods.

--- src/AI.java
+++ src/AI.java
@@ -9,15 +9,23 @@ public class AI {
         playerSymbol = 1;
     }
 
-    public static  int miniMax(int depth, Space[][] board){
+    public static int miniMax(Space[][] board, int depth, int end,boolean maximizing ){
+        // Base statement for when to stop the recursion
         if (isFull() || depth == 0){
             return 0;
         }
-        return 1;
-    }
 
-    public static void copyBoard(Space[][] board){
-        copyOfBoard = board;
+        // Check if the method is maximizing
+        // if it is, iterate through the columns of the board and if it is not an empty space, skip it.
+        // create a copy of the board and apply possible moves in those test boards. And then set the score of those to the miniMax method with a lower depth
+        // check if the score gotten from the recursion is better than the best score, if it is, change best score to score and best move to the column
+        // else if its minimizing, AI wants to get the lowest score for the human
+        // iterate through the columns of the board and if it is not an empty space, skip it.
+        // create a copy of the board and apply possible moves in those test boards. And then set the score of those to the miniMax method with a lower depth
+        // check if the score gotten from the recursion is lower than the best score, if it is, change best score to score
+        // at the end, if depth is reached, return bestMove else if its not, return best score.
+
+        return 1;
     }
 
     public static boolean isFull(){
@@ -35,9 +43,48 @@ public class AI {
         return true;
     }
 
-    public static boolean isValid(int col){
-        return copyOfBoard[copyOfBoard.length - 1][col].getNum() == 0;
+//    public boolean isValid(int col, Space[][] gameBoard){
+//        if (col < 0 || col >= gameBoard[0].length){
+//            return false;
+//        }
+//        return copyOfBoard[copyOfBoard.length - 1][col].getNum() == 0;
+//    }
+
+    public static Space[][] copyBoard(Space[][] board){
+        Space[][] newBoard = new Space[board.length][board[0].length];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                newBoard[i][j] = new Space(board[i][j].getNum());
+            }
+        }
+        return newBoard;
     }
+
+    public void placeMove(Space[][] gameBoard, int col, Player player){
+        for (int i = 0; i < gameBoard.length; i ++) {
+            if (gameBoard[i][col].getNum() == 0){
+                gameBoard[i][col] = new Space(playerSymbol);
+                break;
+            }
+        }
+    }
+
+    // not finished
+    public static int evaluateBoard(Space[][] gameBoard, String aiSymbol, String humanSymbol){
+        int aiCount = 0;
+        int humanCount = 0;
+        for (int c = 2; c <= 4 ; c++) {
+            for (int i = 0; i < gameBoard.length; i++) {
+                if (gameBoard[i][c].getNum() == 2){
+                    aiCount++;
+                } else if (gameBoard[i][c].getNum() == 1){
+                    humanCount++;
+                }
+            }
+        }
+        return 0;
+    }
+
     public int[] getValidLocations(){
         ArrayList<Integer> validLocations = new ArrayList<Integer>();
         for (int i = 0; i < copyOfBoard[0].length; i++) {
@@ -50,6 +97,10 @@ public class AI {
             valid[i] = validLocations.get(i);
         }
         return valid;
+    }
+
+    public static boolean isValid(int col){
+        return copyOfBoard[copyOfBoard.length - 1][col].getNum() == 0;
     }
 
 }
